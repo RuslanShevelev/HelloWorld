683b26ec2bae9f71d61fc1059d93e0a904b4bc20
RuslanShevelev <shevelevrus@gmail.com>
2023-03-27 10:49:48 +0300

    hw 2.3

---
+++ HW 2.3/hw-2.3.js
@@ -0,0 +1,68 @@
+// Задание 1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+// Задание 2
+let iPhoneBorn = "год выпуска первого iPhone";
+alert(iPhoneBorn);
+
+// Задание 3
+let jsAuthorName = "Brendan Eich";
+alert(jsAuthorName);
+
+// Задание 4
+let b = 10, c = 2;
+alert(b + c);
+alert(b - c);
+alert(b * c);
+alert(b / c);
+
+// Задание 5
+let result = 2 ** 5;
+alert(result);
+
+// Задание 6
+a = 9, b = 2;
+alert(a % b);
+
+// Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+// Задание 8
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+// Задание 9.0
+const user = {
+    name: 'Ruslan',
+    age: 42,
+    isAdmin: true
+}
+
+// Задание 9.1
+user['city of residence'] = 'Syktyvkar';
+
+// Задание 9.2
+user.age = 44;
+
+// Задание 9.3
+delete user['city of residence'];
+console.log(user);
+
+// Задание 9.4
+let info = prompt('Какую информацию хотите узнать o пользователе?', 'age');
+alert(user[info]);
+
+// Задание 10
+let userName = prompt('Сообщите ваше имя');
+alert(`Привет ,${userName}!`);
+
